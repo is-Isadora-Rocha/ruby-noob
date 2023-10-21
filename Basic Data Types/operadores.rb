73e111a15883596e4c3ed289b0f530a37a338560
is-isadora-rocha <is.isadora.rocha@gmail.com>
2023-10-21 17:16:57 -0300

    operadores básico

---
+++ Basic Data Types/operadores.rb
@@ -0,0 +1,9 @@
+somar = 1 + 1
+puts "O valor somado é: #{somar}" 
+
+multiplicar = 2 * 2
+puts "O valor multiplicado é: #{multiplicar}"
+
+exponencial = 2 ** 2
+puts "O valor exponencial é: #{exponencial}" 
+
