78e76c0cbea3716eff2fd768c2899922cc121a9d
is-isadora-rocha <is.isadora.rocha@gmail.com>
2023-10-21 17:45:13 -0300

    uso do float

--- Basic Data Types/operadores.rb
+++ Basic Data Types/operadores.rb
@@ -7,3 +7,10 @@ puts "O valor multiplicado é: #{multiplicar}"
 exponencial = 2 ** 2
 puts "O valor exponencial é: #{exponencial}" 
 
+# ao fazer aritmética com dois inteiros em Ruby, o resultado sempre será um inteiro. 
+valorInteiro = 17 / 5    #=> 3, not 3.4
+puts "valorInteiro"
+
+# Para obter uma resposta precisa, basta substituir um dos números inteiros da expressão por um ponto flutuante.
+valorFloat = 17 / 5.0  #=> 3.4
+puts valorFloat
