ff478a7e13c1b97f9937dcb7e8a4c8494afcfbc7
is-isadora-rocha <is.isadora.rocha@gmail.com>
2023-10-26 10:52:24 -0300

    funcoes

--- Basic Data Types/basics.rb
+++ Basic Data Types/basics.rb
@@ -1,11 +1,16 @@
 class Person
-    def initialize(name, age)
-        //variaveis globais
+    def initialize(name, age) #função/método
+        #variaveis globais
         @name = name
         @age = age
     end
 
-    def introduce 
+    def introduce #função/método
         "Olá, meu nome é #{@name} e eu tenho #{@age} anos."
+    end
+
+    def adult?
+        @age >= 18
+        #a última do método funciona como retorno
     end
 end 
