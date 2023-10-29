12fd29d7f5c628b57ff98fa5f6387e70a9968461
is-isadora-rocha <is.isadora.rocha@gmail.com>
2023-10-28 21:22:48 -0300

    inicializando variaveis

---
+++ primeiroContato/identifiers.rb
@@ -0,0 +1,18 @@
+# declarando e inicilizando CONSTANTES 
+Con1 = 5
+Con2 = 12
+
+puts "Primeira Constante: #{Con1}"
+puts "Segunda Constante: #{Con2}"
+
+# declarando e inicilizando VARIÁVEIS 
+
+var1 = 5
+var2 = 12
+
+#alterando variaveis
+var1 = 10
+var2 = 20
+
+puts "Primeira Variável: #{var1}"
+puts "Segunda Variável: #{var2}"
